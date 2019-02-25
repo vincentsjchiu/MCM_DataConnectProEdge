ad46233ad1ac9e23ecca3c8b5a655e99aa1dc420
vincent591400 <vincentsj.chiu@adlinktech.com>
2019-02-25 22:55:56 +0800

    data

---
+++ WindowsFormsApplication1/GenericCopier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace MVAFW.TestItemColls
+{
+    public static class GenericCopier<T>    //deep copy a list
+    {
+        public static T DeepCopy(object objectToCopy)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, objectToCopy);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                return (T)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+    }
+}
